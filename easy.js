6f65e02915512b0e366a1e27c1c6b8ab2da9221a
way0750 <bruce0750@gmail.com>
2016-01-20 14:28:34 -0800

    can now accept url to parse job opening

--- easy.js
+++ easy.js
@@ -1,83 +1,119 @@
 var fileSystem = require('fs');
-var requirement = fileSystem.readFileSync('./req.txt', 'utf8').toLowerCase();
+var request = require('request');
 
-var connectingWords = fileSystem.readFileSync('./connectingWords.txt', 'utf8').toLowerCase().match(/.+/g);
-
-var myTechList = fileSystem.readFileSync('./mytechlist.txt', 'utf8').toLowerCase().match(/\w+/g);
-
-var sentenceArr = fileSystem.readFileSync('./sentence.txt', 'utf8').match(/.+/g);
+String.prototype.toCap = function () {
+  return this[0].toUpperCase() + this.slice(1);
+};
 
 
-var techObj = {};
-
-myTechList = myTechList.filter(function (techName) {
-  if (requirement.search(techName) > -1) {
-    techObj[techName] = [];
-    return true;
+var makeMatchingMatrix = function (skillPath, sentencePath, requirementPathOrHTML, isUsingHTML) {
+  var sentenceArr = fileSystem.readFileSync(sentencePath, 'utf8').match(/.+/g);
+  var mySkillArr = fileSystem.readFileSync(skillPath, 'utf8').toLowerCase().match(/\w+/g);
+  var requirement;
+  if (isUsingHTML) {
+    requirement = requirementPathOrHTML;
   } else {
-    return false;
+    requirement = fileSystem.readFileSync(requirementPathOrHTML, 'utf8').toLowerCase();
   }
-});
-
-var sentenceObj = {};
-sentenceArr.forEach(function (sentence, index) {
-  sentenceObj[index] = [];
-  myTechList.forEach(function (techName) {
-    if (sentence.search(new RegExp(techName, 'i')) > -1) {
-      matchingSentence = true;
-      sentenceObj[index].push(techName);
-      techObj[techName].push(index);
+  var skillObj = {};
+  mySkillArr = mySkillArr.filter(function (skillName) {
+    if (requirement.search(new RegExp(skillName, 'i')) > -1) {
+      skillObj[skillName] = [];
+      return true;
+    } else {
+      return false;
     }
   });
-});
+
+  var sentenceObj = {};
+  sentenceArr.forEach(function (sentence, index) {
+    sentenceObj[index] = [];
+    mySkillArr.forEach(function (skillName) {
+      if (sentence.search(new RegExp(skillName, 'i')) > -1) {
+        sentenceObj[index].push(skillName);
+        skillObj[skillName].push(index);
+      }
+    });
+  });
+
+  mySkillArr = mySkillArr.sort(function (skill1, skill2) {
+    return skillObj[skill1].length - skillObj[skill2].length;
+  });
 
 
-var myTechList = myTechList.sort(function (tech1, tech2) {
-  return techObj[tech1].length - techObj[tech2].length;
-});
+  return {
+    skillObj: skillObj,
+    skillArr: mySkillArr,
+    sentenceObj: sentenceObj,
+    sentenceArr: sentenceArr
+  };
+};
 
 function random (arr) {
   var index = Math.floor(Math.random() * arr.length);
   return arr[index];
 }
 
-var finalSentences = myTechList.reduce(function (arr, techName) {
-  if (!techObj[techName]) {
-    return arr;
-  }
+function selectSentence (skillPath, sentencePath, requirementPathOrHTML, isUsingHTML) {
+  var techObj, sentenceObj, matrix;
+  matrix = makeMatchingMatrix(skillPath, sentencePath, requirementPathOrHTML, isUsingHTML);
 
-  var stillExistSentences = techObj[techName].filter(function(sentenceID) {
-    return sentenceObj[sentenceID];
-  });
+  techObj     = matrix.skillObj;
+  myTechList  = matrix.skillArr;
+  sentenceObj = matrix.sentenceObj;
+  sentenceArr = matrix.sentenceArr;
   
-  if (stillExistSentences.length === 0 ) {
-    arr.push('for ' + techName + ' you still need to write a sentence@@@@@@@@@@@@@@@@@@@@@@@@@');
-    return arr;
-  }
-  
-  var randomSentenceID = random(stillExistSentences);
+  var finalSentences = myTechList.reduce(function (arr, techName) {
+    if (!techObj[techName]) {
+      return arr;
+    }
 
-  arr.push(sentenceArr[randomSentenceID]);
-
-  sentenceObj[randomSentenceID].forEach(function (techName) {
-    //go through each tech and delete all of its sentenceID from the sentenceObj;
-    //then delete itself form the techObj;
-    techObj[techName].forEach(function (sentenceID) {
-      delete sentenceObj[sentenceID];
+    var stillExistSentences = techObj[techName].filter(function(sentenceID) {
+      return sentenceObj[sentenceID];
     });
-    delete techObj[techName];
+    
+    if (stillExistSentences.length === 0 ) {
+      arr.push('\n\n\t!!!!!for ' + techName + ' you still need to write a sentence@@@@@@@@@@@@@@@@@@@@@@@@@\n\n');
+    } else {
+      var randomSentenceID = random(stillExistSentences);
+      var sentence = sentenceArr[randomSentenceID];
+      arr.push(sentence);
+
+      sentenceObj[randomSentenceID].forEach(function (techName) {
+        //go through each tech and delete all of its sentenceID from the sentenceObj;
+        //then delete itself form the techObj;
+        techObj[techName].forEach(function (sentenceID) {
+          delete sentenceObj[sentenceID];
+        });
+        delete techObj[techName];
+      });
+    }
+
+    return arr;
+
+  }, []);
+
+  return finalSentences;
+
+}
+
+function formatSentence (sentenceArr, connectingWordsPath) {
+
+  var connectingWords = fileSystem.readFileSync(connectingWordsPath, 'utf8').toLowerCase().match(/.+/g);
+  sentenceArr.forEach(function (sentence, index) {
+    sentenceArr[index] = connectingWords[index].toCap() + ', ' + sentence;
   });
+  return sentenceArr.join(' ').toCap();
+}
 
-  return arr;
+var url = process.argv[2];
 
-}, []);
-
-String.prototype.toCap = function () {
-  return this[0].toUpperCase() + this.slice(1);
-};
-
-finalSentences.slice(1).forEach(function (sentence, index) {
-  finalSentences[index+1] = connectingWords[index].toCap() + ', ' + sentence;
-});
-
-console.log(finalSentences.join(' ').toCap());
+if (url) {
+  request(url, function (error, data) {
+    var finalSentences = selectSentence('./mytechlist.txt', 'sentence.txt', data.body, true);
+    console.log(formatSentence(finalSentences, './connectingWords.txt'));  
+  });
+} else {
+  var finalSentences = selectSentence('./mytechlist.txt', 'sentence.txt', './req.txt');
+  console.log(formatSentence(finalSentences, './connectingWords.txt'));  
+}
